a2ab149df4402030248bb6346151d2ba9bd178a0
Valeryia Tselesh <valeryiatselesh@gmail.com>
2024-06-07 11:57:05 +0300

    feat: add Hover Animation Pause and Slider Touch

--- coffee-shop/js/script.js
+++ coffee-shop/js/script.js
@@ -40,14 +40,14 @@ function burgerMenu() {
 
 //Slider
 let slides = document.querySelectorAll('.slider__item'),
-   slidesLine = document.querySelector('.slider__items'),
-   slidesPag = document.querySelectorAll('.slider__control'),
-   prevButton = document.querySelector('.slider__arrow-prev'),
-   nextButton = document.querySelector('.slider__arrow-next'),
-   pagLine = document.querySelectorAll('.slider__control-line');
+    slidesLine = document.querySelector('.slider__items'),
+    slidesPag = document.querySelectorAll('.slider__control'),
+    prevButton = document.querySelector('.slider__arrow-prev'),
+    nextButton = document.querySelector('.slider__arrow-next'),
+    pagLine = document.querySelectorAll('.slider__control-line');
 
 let sliderCount = 0,
-   sliderWidth;
+    sliderWidth;
 
 window.addEventListener('resize', showSlide);
 function showSlide() {
@@ -89,3 +89,53 @@ slidesPag.forEach((item) => {
 });
 prevButton.addEventListener('click', prevSlide);
 nextButton.addEventListener('click', nextSlide);
+
+//Hover Animation Pause
+function pauseAnimation() {
+   let pagLineActive = pagLine[sliderCount];
+   pagLineActive.style.animationPlayState = 'paused';
+}
+function startAnimation() {
+   let pagLineActive = pagLine[sliderCount];
+   pagLineActive.style.animationPlayState = 'running';
+}
+slides.forEach((item) => {
+   item.addEventListener('touchstart', pauseAnimation);
+   item.addEventListener('touchend', startAnimation);
+});
+slides.forEach((item) => {
+   item.addEventListener('mouseover', pauseAnimation);
+   item.addEventListener('mouseout', startAnimation);
+});
+
+// Slider Touch
+
+let x1 = null,
+    y2 = null;
+
+function handleTouchStart(event) {
+   const firstTouch = event.touches[0];
+   x1 = firstTouch.clientX;
+   y1 = firstTouch.clientY;
+}
+function handleTouchMove(event) {
+   if (!x1 || !y1) {
+      return false;
+   }
+   let x2 = event.touches[0].clientX,
+      y2 = event.touches[0].clientY;
+
+   let xRes = x2 - x1,
+      yRes = y1 - y2;
+
+   if (Math.abs(xRes) > Math.abs(yRes)) {
+      if (xRes > 0) prevSlide();
+      else nextSlide()
+   }
+   x1 = null;
+   y1 = null;
+}
+slides.forEach((slide) => {
+   slide.addEventListener('touchstart', handleTouchStart, false);
+   slide.addEventListener('touchmove', handleTouchMove, false);
+})
